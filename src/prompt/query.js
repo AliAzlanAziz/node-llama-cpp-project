b82c3ed50d0ccaab53b1f0b3fcd0efc59df3885e
AliAzlanAziz <ali@retrocausal.ai>
2024-07-08 00:38:47 +0500

    actual prompts text

---
+++ src/prompt/query.js
@@ -0,0 +1,17 @@
+export const eduQuery = {
+  verbs: 'Count the number of verbs in the given text. If a verb is being repeated one or multiple times then count it as many times as it is occuring, do not write any text in the answer except for just the count.',
+  nouns: 'Count the number of tools/method/skills/techniques/technology or any such nouns in the given text. If tools/method/skills/techniques/technology or any such noun is being repeated one or multiple times then count it as many times as it is occuring, do not write any text in the answer except for just the count.',
+  degree: 'Count the number of degree/course/subject/field that sounds professional in the given text. Do not write any text in the answer except for just the count.',
+  school: 'Count the number of reputed school names in the given text. Do not write any text in the answer except for just the count.'
+}
+
+export const expQuery = {
+  verbs: 'Count the number of verbs in the given text. If a verb is being repeated one or multiple times then count it as many times as it is occuring, do not write any text in the answer except for just the count.',
+  nouns: 'Count the number of tools/method/skills/techniques/technology or any such nouns in the given text. If tools/method/skills/techniques/technology or any such noun is being repeated one or multiple times then count it as many times as it is occuring, do not write any text in the answer except for just the count.',
+  degree: 'Count the number of degree/course/subject/field that sounds professional in the given text. Do not write any text in the answer except for just the count.',
+  school: 'Count the number of reputed school names in the given text. Do not write any text in the answer except for just the count.'
+}
+
+export const proj = ''
+export const cert = ''
+export const award = ''
