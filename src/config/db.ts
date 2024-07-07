cf8498cdef006f9eb91f756a63c7ffe4f3e9070f
AliAzlanAziz <ali@retrocausal.ai>
2024-07-08 00:37:15 +0500

    config files for the packages and services

---
+++ src/config/db.ts
@@ -0,0 +1,43 @@
+import mongoose, { connect } from "mongoose";
+import { serviceLogger } from "./logger";
+
+const logger = serviceLogger('db.ts');
+
+//function to connect to database
+const connectDB = async () => {
+    try{
+        const conn = await connect(process.env.MONGO_URI as string)
+
+        logger.info(`MongoDB connected at host: ${conn.connection.host}`)
+    } catch(err) {
+        logger.info(err)
+        process.exit(1)
+    }
+}
+
+mongoose.connection.on('connected', () => {
+    logger.info('Mongoose connected!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    logger.info('Mongoose disconnected!');
+});
+
+// Function to gracefully close the Mongoose connection
+const gracefulExit = async () => {
+    await mongoose.connection.close();
+    logger.info('Mongoose connection closed through app termination');
+    process.exit(0);
+};
+
+const signals = ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'];
+
+signals.forEach(signal => {
+    process.on(signal, gracefulExit);
+});
+
+export default connectDB
---
+++ src/config/logger.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import winston from 'winston'
+
+const date = new Date().toLocaleDateString().replaceAll('/','_',);
+const time = new Date().toLocaleTimeString().replaceAll(' ','-').replaceAll(':','_');
+const filename = `${date}_${time}.txt`
+console.log('filename: '+filename)
+
+const mainLogger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(), 
+    winston.format.json()
+  ),
+  transports: [
+    new (winston.transports.Console)({
+      format: winston.format.colorize({all: true})
+    }),
+    new winston.transports.File({
+      filename: path.join(__dirname, '..', `/logs/${filename}`)
+    }),
+  ],
+});
+
+export const serviceLogger = (service: string) => mainLogger.child({ service: service })
